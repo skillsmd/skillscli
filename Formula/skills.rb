8386672ca9b83de0bf7ba4afea03bed3aebd0c8d
HUIKE <wubang.huike@gmail.com>
2025-12-23 11:43:18 +0800

    fix: update SHA256 checksums for macOS and Linux binaries in Homebrew formula

--- Formula/skills.rb
+++ Formula/skills.rb
@@ -6,16 +6,16 @@ class Skills < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/skillsmd/skillscli/releases/download/v0.1.0/skills-macos-aarch64.tar.gz"
-      sha256 "REPLACE_WITH_ACTUAL_SHA256"
+      sha256 "02314e9e5ecf55ab7f75f984cc956545e0cf3ac0bb54ec6f722b5ca12e2f4797"
     else
       url "https://github.com/skillsmd/skillscli/releases/download/v0.1.0/skills-macos-x86_64.tar.gz"
-      sha256 "REPLACE_WITH_ACTUAL_SHA256"
+      sha256 "02314e9e5ecf55ab7f75f984cc956545e0cf3ac0bb54ec6f722b5ca12e2f4797"
     end
   end
 
   on_linux do
     url "https://github.com/skillsmd/skillscli/releases/download/v0.1.0/skills-linux-x86_64.tar.gz"
-    sha256 "REPLACE_WITH_ACTUAL_SHA256"
+    sha256 "02314e9e5ecf55ab7f75f984cc956545e0cf3ac0bb54ec6f722b5ca12e2f4797"
   end
 
   def install
